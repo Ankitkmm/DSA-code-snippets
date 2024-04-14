73580afa2cfc8a0c4d70cff107e58219961af700
Ankit Kumar <91867197+Ankitkmm@users.noreply.github.com>
2024-04-14 14:09:55 +0530

    Update dsa.cpp

--- dsa.cpp
+++ dsa.cpp
@@ -221,7 +221,8 @@ Number Theory
 // Primality Test
 bool prime(int n) {
     if (n < 2) return 0;
-    for (int i = 2; i * i <= n; i++) {
+    if(!(n&1)) return 0;
+    for (int i = 3; i * i <= n; i+=2) {
         if (n % i == 0) return 0;
     }
     return 1;
